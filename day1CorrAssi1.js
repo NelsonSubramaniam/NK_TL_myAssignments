06a4374e3633094ac144043221c03dad9baa491c
NelsonSubramaniam <101327478+NelsonSubramaniam@users.noreply.github.com>
2025-11-22 13:39:52 +0530

    Add files via upload

---
+++ day1CorrAssi1.js
@@ -0,0 +1,18 @@
+
+let firstName= "Nelson"
+console.log("The data type of firstName is "+ typeof firstName)
+
+let companyName= "Radian"
+console.log("The data type of companyName is "+ typeof companyName)
+
+let mobileNumber= 9176169310
+console.log("The data type of mobileNumber is "+typeof mobileNumber)
+
+let isAutomation=true
+console.log("the data type of isAutomation is " + typeof isAutomation)
+
+let hasPlaywright
+console.log("the data type of hasPlaywight is " + typeof hasPlaywright)
+
+let landLine= null
+console.log("the data type of landLine is "+ typeof landLine)
